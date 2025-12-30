6430f615068c128dc1bcc281c0bb56f7d617177a
Haruto Morita <hmorita@c3r4s1.42tokyo.jp>
2025-12-30 16:39:39 +0900

    refactored and added functions

--- src/checker/checker_utils.c
+++ src/checker/checker_utils.c
@@ -6,7 +6,7 @@
 /*   By: hmorita <hmorita@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/26 05:19:38 by hmorita           #+#    #+#             */
-/*   Updated: 2025/12/30 15:48:48 by hmorita          ###   ########.fr       */
+/*   Updated: 2025/12/30 16:08:57 by hmorita          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ void	free_all(t_stack **stk, char **strs, bool is_split)
 
 int	error_handle(t_stack **stk, char **strs, bool is_split)
 {
-	write(2, "Error\n", 6);
 	free_all(stk, strs, is_split);
 	return (1);
 }
